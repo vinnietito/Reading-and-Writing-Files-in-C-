fbbcccb1d051aa676a70292a00452960009ef143
vinnietito <vincentnjuguna69@gmail.com>
2024-09-19 21:18:20 +0300

    I made some changes to the file

--- ReadingWritingFiles.cpp
+++ ReadingWritingFiles.cpp
@@ -2,30 +2,38 @@
 #include <fstream>  // For file input/output
 #include <string>   // For string handling
 
-int main() {
+int main()
+{
     // Writing to a file
-    std::ofstream outputFile("example.txt"); // Create an output file stream
+    std::ofstream outputFile("example.txt"); 
     
-    if (outputFile.is_open()) {
+    if (outputFile.is_open())
+	{
         outputFile << "Hello, this is a test file.\n";
         outputFile << "Writing some data to the file.\n";
-        outputFile.close();  // Always close the file when done
+        outputFile.close(); 
         std::cout << "Data written to file successfully.\n";
-    } else {
+    } 
+	else
+	{
         std::cerr << "Unable to open file for writing.\n";
     }
 
     // Reading from a file
-    std::ifstream inputFile("example.txt"); // Create an input file stream
+    std::ifstream inputFile("example.txt"); 
     std::string line;
     
-    if (inputFile.is_open()) {
+    if (inputFile.is_open())
+	{
         std::cout << "Reading from file:\n";
-        while (getline(inputFile, line)) {  // Read line by line
+        while (getline(inputFile, line))
+		{  
             std::cout << line << '\n';
         }
-        inputFile.close();  // Always close the file when done
-    } else {
+        inputFile.close(); 
+    } 
+	else 
+	{
         std::cerr << "Unable to open file for reading.\n";
     }
 
